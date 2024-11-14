8e69d95d38b4694d490462cc5e0a25fead7aa4e3
Houssam <h.lafkih@gmail.com>
2024-11-14 22:13:11 +0100

    First Version of Modern Design of the Website

--- pf-app/src/App.svelte
+++ pf-app/src/App.svelte
@@ -1,131 +1,184 @@
 <script>
 	let homeLink = "/";
+	const changeColorAndText = (element) => {
+		let div = document.querySelector("." + element);
+		let text = document.querySelector("." + element + " h2");
+		div.style.transitionProperty = "background";
+		text.style.transitionProperty = "font-size";
+		text.style.fontSize = "28px";
+		text.style.transitionDuration = "0.3s";
+		div.style.transitionDuration = "0.618s";
+		div.style.background = "purple";
+	};
+	const changeBackColorAndText = (element) => {
+		let div = document.querySelector("." + element);
+		let text = document.querySelector("." + element + " h2");
+		text.style.fontSize = "24px";
+		div.style.background = "none";
+	};
+	const changeBackColor = (element) => {
+		let div = document.querySelector("." + element);
+		div.style.background = "#160022";
+	};
 	const changeColor = (element) => {
 		let div = document.querySelector("." + element);
 		div.style.transitionProperty = "background";
 		div.style.transitionDuration = "0.618s";
-		div.style.background = "#666666";
-	};
-	const changeBackColor = (element) => {
-		let div = document.querySelector("." + element);
-		div.style.background = "none";
+		div.style.background = "#320040";
 	};
 </script>
 
 <body>
-	<div class="header">
-		<h1 class="name">Houssam Lafkih</h1>
-		<div
-			class="home"
-			id="home"
-			on:mouseover={() => {
-				changeColor("home");
-			}}
-			on:mouseleave={() => {
-				changeBackColor("home");
-			}}
-			on:focus
-		>
-			<h2><a href={homeLink}>Home</a></h2>
+	<div class="firstscreen">
+		<div class="header">
+			<h1 class="name">Houssam Lafkih</h1>
+			<div
+				class="home"
+				id="home"
+				on:mouseover={() => {
+					changeColorAndText("home");
+				}}
+				on:mouseleave={() => {
+					changeBackColorAndText("home");
+				}}
+				on:focus
+			>
+				<h2><a href={homeLink}>Home</a></h2>
+			</div>
+			<div
+				class="blog"
+				on:mouseover={() => {
+					changeColorAndText("blog");
+				}}
+				on:mouseleave={() => {
+					changeBackColorAndText("blog");
+				}}
+				on:focus
+			>
+				<h2><a href="/blog">Blog</a></h2>
+			</div>
+			<div
+				class="github"
+				on:mouseover={() => {
+					changeColorAndText("github");
+				}}
+				on:mouseleave={() => {
+					changeBackColorAndText("github");
+				}}
+				on:focus
+			>
+				<h2>
+					<a href="https://github.com/houssamlfk/" target="_blank"
+						>Github</a
+					>
+				</h2>
+			</div>
+			<div class="resume">
+				<button class="resumebtn">Resume</button>
+			</div>
 		</div>
-		<div
-			class="blog"
-			on:mouseover={() => {
-				changeColor("blog");
-			}}
-			on:mouseleave={() => {
-				changeBackColor("blog");
-			}}
-			on:focus
-		>
-			<h2><a href="/blog">Blog</a></h2>
-		</div>
-		<div
-			class="github"
-			on:mouseover={() => {
-				changeColor("github");
-			}}
-			on:mouseleave={() => {
-				changeBackColor("github");
-			}}
-			on:focus
-		>
-			<h2>
-				<a href="https://github.com/houssamlfk/" target="_blank"
-					>Github</a
-				>
-			</h2>
+		<div class="presentation">
+			<div class="presentationitems">
+				<div class="about">
+					<h1>
+						Designing responsive websites that suit your taste and
+						needs.
+					</h1>
+				</div>
+			</div>
 		</div>
 	</div>
-	<div class="presentation">
-		<div class="imgplaceholder">
-			<p>This is a placeholder for an image</p>
+	<div
+		class="techstack"
+		on:mouseover={() => {
+			changeColor("techstack");
+		}}
+		on:focus
+		on:mouseleave={() => {
+			changeBackColor("techstack");
+		}}
+	>
+		<div class="techstackheader">
+			<h1 class="techstacktitle">My Techstack</h1>
+			<div class="techstacktrait">00000000000</div>
 		</div>
-		<div class="about">
-			<h1>I'm Houssam Lafkih, a Website Developer!</h1>
-			<p>
-				I am a passionate software engineer, I got into this domain by
-				building simples flash games, and now I'm exploring the world of
-				Web Development
-			</p>
-		</div>
-	</div>
-	<div class="techstackheader">
-		<h1 class="techstacktitle">My Techstack</h1>
-	</div>
-	<div class="techstack">
 		<div class="techitems">
-			<div class="html">
-				<h1>HTML</h1>
-				<img
-					alt="html5"
-					src="vendor/html.png"
-					height="128px"
-					width="128px"
-				/>
+			<div class="row1">
+				<div class="html">
+					<img
+						alt="html5"
+						src="vendor/html.svg"
+						height="128px"
+						width="128px"
+					/>
+					<h1>HTML</h1>
+				</div>
+				<div class="css">
+					<img
+						alt="css"
+						src="vendor/css.svg"
+						height="128px"
+						width="128px"
+					/>
+					<h1>CSS</h1>
+				</div>
+				<div class="javascript">
+					<img
+						alt="javascript"
+						src="vendor/js.svg"
+						height="128px"
+						width="128px"
+					/>
+					<h1>Javascript</h1>
+				</div>
 			</div>
-			<div class="css">
-				<h1>CSS</h1>
-				<img
-					alt="css"
-					src="vendor/css.png"
-					height="128px"
-					width="128px"
-				/>
-			</div>
-			<div class="express">
-				<h1>expressjs</h1>
-				<img
-					alt="expressjs"
-					src="vendor/express.png"
-					height="128px"
-					width="256px"
-				/>
-			</div>
-			<div class="svelte">
-				<h1>svelte</h1>
-				<img
-					alt="svelte"
-					src="vendor/svelte.png"
-					height="128px"
-					width="128px"
-				/>
-			</div>
-			<div class="sqlite">
-				<h1>Sqlite</h1>
-				<img
-					alt="sqlite"
-					src="vendor/sqlite.png"
-					height="128px"
-					width="128px"
-				/>
+			<div class="row2">
+				<div class="express">
+					<img
+						alt="expressjs"
+						src="vendor/express.svg"
+						height="128px"
+						width="128px"
+					/>
+					<h1>expressjs</h1>
+				</div>
+
+				<div class="svelte">
+					<img
+						alt="svelte"
+						src="vendor/svelte.svg"
+						height="128px"
+						width="128px"
+					/>
+					<h1>svelte</h1>
+				</div>
+				<div class="sqlite">
+					<img
+						alt="sqlite"
+						src="vendor/sqlite.png"
+						height="128px"
+						width="128px"
+					/>
+					<h1>Sqlite</h1>
+				</div>
 			</div>
 		</div>
 	</div>
-	<div class="projectsheader">
-		<h1>Projects I've worked on</h1>
-	</div>
-	<div class="projects">
+
+	<div
+		class="projects"
+		on:mouseover={() => {
+			changeColor("projects");
+		}}
+		on:focus
+		on:mouseleave={() => {
+			changeBackColor("projects");
+		}}
+	>
+		<div class="projectsheader">
+			<h1>Projects I've worked on</h1>
+			<div>00000000</div>
+		</div>
 		<div class="firstwebsite">
 			<div class="prodcorner">
 				<h1>Productivity Corner</h1>
@@ -157,10 +210,20 @@
 			</div>
 		</div>
 	</div>
-	<div class="contactheader">
-		<h1>Contact Me!</h1>
-	</div>
-	<div class="contact">
+	<div
+		class="contact"
+		on:mouseover={() => {
+			changeColor("contact");
+		}}
+		on:mouseleave={() => {
+			changeBackColor("contact");
+		}}
+		on:focus
+	>
+		<div class="contactheader">
+			<h1>Contact Me!</h1>
+			<div>000000000</div>
+		</div>
 		<div class="email">
 			<img
 				alt="email"
@@ -187,21 +250,9 @@
 
 <style>
 	.header {
-		position: fixed;
 		display: flex;
-		top: 0;
-		left: 0;
-		right: 0;
 		align-items: center;
 		padding: 0px 24px 0px 24px;
-		background: linear-gradient(
-			rgba(34, 34, 34, 1),
-			rgba(34, 34, 34, 0.99),
-			rgba(34, 34, 34, 0.98),
-			rgba(34, 34, 34, 0.95),
-			rgba(34, 34, 34, 0.8),
-			rgba(34, 34, 34, 0.5)
-		);
 		margin-right: auto;
 		margin-left: auto;
 	}
@@ -217,78 +268,147 @@
 	.name {
 		margin-right: auto;
 	}
+	.header h1 {
+		padding-top: 6px;
+		font-weight: 500;
+		color: #d690fc;
+		font-family:
+			system-ui,
+			-apple-system,
+			BlinkMacSystemFont,
+			"Segoe UI",
+			Roboto,
+			Oxygen,
+			Ubuntu,
+			Cantarell,
+			"Open Sans",
+			"Helvetica Neue",
+			sans-serif;
+	}
 	.header a {
-		color: #ede7e6;
+		color: #eedcfe;
 		text-decoration: none;
+	}
+	.resume {
+		padding-top: 8px;
+		margin-left: 50px;
+	}
+	.resumebtn {
+		background-color: cyan;
+		border-radius: 40px;
+		color: #160020;
+		border: transparent;
+	}
+	.firstscreen {
+		background-size: 1920px 1080px;
+		min-height: 100%;
+		background-image: url("/vendor/parallax.jpg");
 	}
 	.presentation {
 		display: flex;
 		flex-direction: row;
 		justify-content: center;
-		background: #571000;
 		gap: 12px;
-		padding-top: 96px;
+		padding-top: 190px;
 		padding-bottom: 12px;
 		color: #ede7e6;
+	}
+	.presentationitems {
+		display: flex;
+		align-items: center;
 	}
 	.presentation h1 {
 		color: #ffe5b4;
 	}
-	.imgplaceholder {
+	.row1 {
 		display: flex;
-		align-items: center;
 		justify-content: center;
-		min-width: 300px;
-		border: 2px black solid;
+		align-items: center;
+		gap: 72px;
+	}
+	.row2 {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 72px;
 	}
 	.about {
+		background: rgba(0, 0, 0, 0);
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-	}
-	.about p {
-		line-height: 35px;
-		font-size: 20px;
-		max-width: 400px;
+		border-radius: 12px;
 	}
 	.about h1 {
-		font-size: 32px;
-		max-width: 400px;
-		text-align: center;
+		text-transform: none;
+		font-weight: 400;
+		background-image: linear-gradient(to left, #94fbff, #ff7df0);
+		color: transparent;
+		background-clip: text;
+		font-size: 55px;
+		max-width: 800px;
 	}
 	.projectsheader {
 		display: flex;
 		justify-content: center;
-		background: #111111;
-		color: #ede7e6;
-		padding: 0;
+		flex-direction: column;
+		padding-bottom: 24px;
+	}
+	.projectsheader h1 {
+		font-size: 40px;
+		color: transparent;
+		background-image: linear-gradient(to left, #94fbff, #ff7df0);
+		background-clip: text;
+	}
+	.projectsheader div {
+		color: transparent;
+		background-image: linear-gradient(to left, #94fbff, #ff7df0);
+		font-size: 8px;
+		line-height: 8px;
 	}
 	.techstack {
-		background: #571000;
-	}
-	.techstackheader {
-		display: flex;
-		justify-content: center;
-		background: #111111;
-		color: #ede7e6;
-	}
-	.projects {
-		background: #571000;
-		color: #ede7e6;
+		background: #160020;
 		display: flex;
 		align-items: center;
 		flex-direction: column;
 	}
-	.projects h1 {
+	.techstackheader {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+		padding-bottom: 24px;
+	}
+	.techstackheader div {
+		color: transparent;
+		background: linear-gradient(to left, #94fbff, #ff7df0);
+		border-radius: 40px;
+		font-size: 8px;
+		line-height: 8px;
+	}
+	.techstackheader h1 {
+		font-size: 40px;
+		color: transparent;
+		background-image: linear-gradient(to left, #94fbff, #ff7df0);
+		background-clip: text;
+	}
+	.projects {
+		background: #160020;
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+	}
+	.firstwebsite,
+	.secondwebsite h1 {
 		text-align: center;
-		color: #ffe5b4;
+		color: #94fbff;
 	}
 	.projects p {
 		max-width: 900px;
 		line-height: 35px;
 		text-align: center;
 		font-size: 20px;
+		color: #eeccee;
 	}
 	.firstwebsite {
 		display: flex;
@@ -299,22 +419,40 @@
 		align-items: center;
 	}
 	.techitems {
-		background: white;
 		text-align: center;
 		display: flex;
+		flex-direction: column;
 		justify-content: center;
+		padding-top: 24px;
+	}
+	.techitems h1 {
+		text-transform: none;
+		color: pink;
 	}
 	.contactheader {
 		display: flex;
 		justify-content: center;
-		background: #111111;
-		color: #ede7e6;
+		flex-direction: column;
+		padding-bottom: 24px;
+	}
+	.contactheader div {
+		color: transparent;
+		background-image: linear-gradient(to left, #94fbff, #ff7df0);
+		border-radius: 40px;
+		font-size: 8px;
+		line-height: 8px;
+	}
+	.contactheader h1 {
+		color: transparent;
+		background-image: linear-gradient(to left, #94fbff, #ff7df0);
+		background-clip: text;
+		font-size: 40px;
 	}
 	.contact {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		background: #571000;
+		background: #160020;
 		color: #ede7e6;
 	}
 	.phone {
@@ -329,16 +467,13 @@
 	footer {
 		display: flex;
 		justify-content: center;
-		background: #222222;
+		background: #000000;
 		color: #dddddd;
 	}
 	footer p {
 		font-size: 13px;
 	}
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 2em;
-		font-weight: 100;
+	body {
+		background: red;
 	}
 </style>
